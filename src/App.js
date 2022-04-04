c9b1a13b2709a305f472fa27f6158530fea49f52
codeuronline <j.kasperski@codeur.online>
2022-04-04 09:30:45 +0200

    update new pages

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import About from './pages/About';
 import Home from './pages/Home';
+import Contact from './pages/Contact';
+
 const container = document.getElementById('root');
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}/>
-        <Route path="/about" element={<About />}/>
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={ <Contact/>}/>
         <Route path="*" element={<About />}/>
       </Routes>
     </BrowserRouter>
---
+++ src/pages/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
+
+const Contact = () => {
+    return (
+        <div>
+            <Logo/>
+            <Navigation />
+            <Form></Form>
+        </div>
+    );
+};
+
+export default Contact;
