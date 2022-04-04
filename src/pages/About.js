40a1cef7842a6e3712b8861fc5e8790b82855136
codeuronline <j.kasperski@codeur.online>
2022-04-04 09:12:50 +0200

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+import Logo from "../components/Logo";
+import Countries from '../components/Countries';
+const About
+ = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt, corrupti?
+                Suscipit perspiciatis placeat beatae voluptatum sit
+                , tempora odit! Obcaecati iure id dignissimos temporibus minima quae,
+                nobis esse molestiae dolores. Consequatur!</p>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt, corrupti?
+                Suscipit perspiciatis placeat beatae voluptatum sit
+                , tempora odit! Obcaecati iure id dignissimos temporibus minima quae,
+                nobis esse molestiae dolores. Consequatur!</p>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt, corrupti?
+                Suscipit perspiciatis placeat beatae voluptatum sit
+                , tempora odit! Obcaecati iure id dignissimos temporibus minima quae,
+                nobis esse molestiae dolores. Consequatur!</p>
+            
+        </div>
+    );
+};
+
+export default About
+;
